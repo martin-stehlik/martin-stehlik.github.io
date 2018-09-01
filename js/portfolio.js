6947c7c9a8f072cc56b8f2bada86356044d44701
Martin Stehlík <mn.stehlik@seznam.cz>
2018-09-01 15:19:57 +0200

    ui improvements

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,56 +1,43 @@
-/*Smooth scrolling - pure JS solution - for Chrome and Firefox only */
-
-// let navLinks = document.querySelectorAll("nav a");
-
-// for(let i = 0; i < navLinks.length; i++){
-// 	navLinks[i].addEventListener("click", function(event) {
-// 		// prevent direct jump to #
-// 		event.preventDefault();
-// 		let targetId = this.getAttribute("href");
-// 		let target = document.querySelector(targetId);
-// 		target.scrollIntoView({
-// 			block: "start",
-// 			behavior: "smooth"
-// 		});
-// 	});
-// }
-
-
 /* Smooth scroll with jQuery */ 
-$("nav a").click(function() {
-    var targetId = $(this).attr("href");   // save href attr of the anchor tag 
-    $("html, body").animate({scrollTop: $(targetId).offset().top - 20}, 700); // animate html (and body) scrollTop property to the offset().top position of the target 
+$('a[href*="#"]').click(function(){  
+	// save href attr of the anchor tag
+	  let targetId = $(this).attr("href");    
+	// animate html and body scrollTop property to the offset().top position of the target
+	  $("html, body").animate({scrollTop: $(targetId).offset().top - 20}, 700);  
     return false; // prevent default direct jump and event bubbling  
 });
 
-/* show/hide the sidebar */ 
-var toggler = document.querySelector("#js-toggler");
-var togglerIcon = document.querySelector("#js-toggler i")
-var menu = document.querySelector("#js-menu");
+ /* show/hide the sidebar */
+let toggler = document.querySelector("#js-toggler");
+let togglerIcon = document.querySelector("#js-toggler i");
+let menu = document.querySelector("#js-menu");
+let menuLinks = document.querySelectorAll(".menu a");
 
-toggler.addEventListener("click", function() {
-		// show/hide sidebar 
-		menu.classList.toggle("menu-hidden");
-		// change toggler color 
-		this.classList.toggle("toggler-on")
-		// change toggler icon 
-		switch (togglerIcon.getAttribute("class")){
-			case "fas fa-bars":
-			  togglerIcon.setAttribute("class", "fas fa-times");
-				break;
-			case "fas fa-times": 
-			  togglerIcon.setAttribute("class", "fas fa-bars");
-				break;
-		}
-});
+for(let i=0; i<menuLinks.length; i++){
+	menuLinks[i].addEventListener("click", toggleSidebar);
+};
+toggler.addEventListener("click", toggleSidebar);
+
+/* show current year in the footer */ 
+let timeNow = new Date;
+let currentYearEl = document.getElementById("js-current-year");
+currentYearEl.textContent = timeNow.getFullYear();
 
 
+// functions 
 
-
-
-
-
-
-
-
-
+function toggleSidebar(){
+	// show/hide sidebar 
+	menu.classList.toggle("menu-hidden");
+	// change toggler color 
+	toggler.classList.toggle("toggler-on")
+	// change toggler icon 
+	switch (togglerIcon.getAttribute("class")){
+		case "fas fa-bars":
+			togglerIcon.setAttribute("class", "fas fa-times");
+			break;
+		case "fas fa-times": 
+			togglerIcon.setAttribute("class", "fas fa-bars");
+			break;
+	}
+}
